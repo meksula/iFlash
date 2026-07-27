6245e7a4a449b4c8d12fafb4493fa355c08c28e6
meksula <meksula.karol.93@gmail.com>
2026-07-28 00:28:40 +0200

    Java 25 bump and all services aligned to this version

--- iflash/helpers/broker-platform/src/test/java/com/iflash/brokerplatform/BrokerPlatformApplicationTests.java
+++ iflash/helpers/broker-platform/src/test/java/com/iflash/brokerplatform/BrokerPlatformApplicationTests.java
@@ -2,8 +2,10 @@ package com.iflash.brokerplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BrokerPlatformApplicationTests {
 
     @Test
